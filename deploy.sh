f75b32f4e9d88ceed07376515419f27db89c038f
Victor Muchikon <muchikon@outlook.com>
2021-01-26 17:01:18 -0500

    Se modifico deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker-compose down
+#docker-compose down
 docker kill gnosis.web
 #docker rm gnosis.web
 docker network create ms
 docker-compose -p gnosis.web up -d
 sleep 10
-docker-compose down
+#docker-compose down
 docker system prune -f
