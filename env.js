1cdb4b8f51a865c30e511007b3d3c5c5517bdb2e
Noheroes <rporrasz@hotmail.com>
2021-01-26 15:59:28 -0500

    Create env.js

---
+++ env.js
@@ -0,0 +1,14 @@
+(function (window) {
+   window.__env = window.__env || {};
+   window.__env.production = true;
+   window.__env.linu_licApi_presentacionUrl = 'http://localhost:59524';
+   window.__env.linu_punku_api_changePasswordUrl = 'https://auth-app.sunedu.gob.pe:4438/Home/ChangePassword?token={0}&returnUrl={1}';
+   window.__env.linu_punku_api_loginUrl = 'https://auth-app.sunedu.gob.pe:4438/Home/Login?userSystemId={0}&returnUrl={1}&extra=';
+   window.__env.linu_punku_api_logoutUrl = 'https://auth-app.sunedu.gob.pe:4438/Home/Logout?token={0}&returnUrl={1}';
+   window.__env.linu_punku_guidSistema = '01E002E4-2794-45AB-A9E3-94FEAC502550';
+   window.__env.linu_firma_accion = 'https://tomcat104.sunedu.gob.pe:8443/SignnetSignature/SignnetFirmaServicio';
+   window.__env.linu_firma_urlConfigService = 'https://tomcat104.sunedu.gob.pe:8443/SignnetSignature/configuracion';
+   window.__env.linu_firma_webService = 'https://tomcat104.sunedu.gob.pe:8443/SignnetSignature/FirmaDigitalWs?wsdl';
+   window.__env.linu_firma_rutaDestino = '/mnt/galenusdesa/destino/';
+   window.__env.linu_firma_rutaOrigen = '/mnt/galenusdesa/origen/';
+})(this)
